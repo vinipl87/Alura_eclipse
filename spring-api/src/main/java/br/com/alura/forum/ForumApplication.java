3df5d59f3017ad000a1b3c5160fe4b6f8a8e70da
Vinícius Lazzeris <vinipl87@gmail.com>
2021-08-04 20:27:04 -0300

    spring api testing

--- spring-api/src/main/java/br/com/alura/forum/ForumApplication.java
+++ spring-api/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,6 +2,8 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -11,10 +13,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
-public class ForumApplication {
+public class ForumApplication /*extends SpringBootServletInitializer*/ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
+	
+	//Em comentário configs para deploy como war
+	
+	/*@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ForumApplication.class);
+	}*/
+	
+	//Maven:
+	//<packaging>war
+	//tomcat provided
+	
+	//docker build –t alura/forum
+	//docker run -p 8080:8080 -e FORUM_DATABASE_URL='jdbc:h2:mem:alura-forum' -e FORUM_DATABASE_USERNAME='sa' -e FORUM_DATABASE_PASSWORD='' -e FORUM_JWT_SECRET='123456' alura/forum
 
 }
